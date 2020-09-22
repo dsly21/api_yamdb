d77c896527d783e4bda326b9befd4be7a0daf872
Nikolay_Zelyukov <nikolaizelyukov@yandex.ru>
2020-09-22 19:18:02 +1000

    add models: Comments, Reviews

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Reviews(models.Model):
+    text = models.TextField()
+    score = models.IntegerField()
+    author = models.ForeignKey(
+                                User, on_delete=models.CASCADE,
+                                related_name="reviews")
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Comments(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+                                User, on_delete=models.CASCADE,
+                                related_name="comments")
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+
+    def __str__(self):
+        return self.text
