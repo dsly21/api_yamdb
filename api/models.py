3be01f9e41671a2a62b520a7d9ad59f6eb9ab727
Nikolay_Zelyukov <nikolaizelyukov@yandex.ru>
2020-09-23 12:11:53 +1000

    add related fields in Comments, Reviews

--- api/models.py
+++ api/models.py
@@ -8,6 +8,8 @@ class Reviews(models.Model):
                                 User, on_delete=models.CASCADE,
                                 related_name="reviews")
     pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+    title = models.ForeignKey(Title, on_delete=models.SET_NULL,
+                              blank=True, null=True, related_name="reviews")
 
     def __str__(self):
         return self.text
@@ -19,6 +21,12 @@ class Comments(models.Model):
                                 User, on_delete=models.CASCADE,
                                 related_name="comments")
     pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+    title = models.ForeignKey(
+                                Title, on_delete=models.SET_NULL,
+                                blank=True, null=True, related_name="comments")
+    review = models.ForeignKey(
+                                Title, on_delete=models.SET_NULL,
+                                blank=True, null=True, related_name="comments")
 
     def __str__(self):
         return self.text
