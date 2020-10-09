a6edd6d6f286074a657fc64e3b0b116110bfce48
Denis S <makafonov@gmail.com>
2020-10-09 16:31:39 +0500

    Updating permissions

--- api/permissions.py
+++ api/permissions.py
@@ -3,9 +3,10 @@ from rest_framework import permissions
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.method in permissions.SAFE_METHODS or request.user and
-            request.user.is_staff and request.user.is_authenticated
+        return (
+            request.method in permissions.SAFE_METHODS or
+            request.user.is_staff and
+            request.user.is_authenticated
         )
 
 
